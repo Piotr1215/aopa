a6b06d036609f4fe3c66d1d60732f20f57ce9158
Piotr Zaniewski <piotr@upbound.io>
2022-10-31 23:42:59 +0100

    Todos

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use std::process::Command;
+use std::{path::Path, process::Command};
 
 fn main() {
+    println!("{}", Path::new("/usr/bin/seda").exists());
+    // TODO: Error handling for executables
     let output = Command::new("/usr/bin/yq")
         .arg("eval")
         .arg(".window.opacity")
+        // TODO: make sure file exists
         .arg("costam.yml")
         .output()
+        // TODO: Error handling with `Result`
         .expect("failed to execute process");
 
     println!("Opacity: {}", String::from_utf8_lossy(&output.stdout));
