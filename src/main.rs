af7887aeee7affc8142ee3873044876e1351b205
Piotr Zaniewski <piotr@upbound.io>
2022-10-31 23:31:26 +0100

    This is honestly the least amount of hassle for a scripting task

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,19 @@
-use std::io::{self, Seek};
+use std::process::Command;
 
-use serde_yaml::Value;
+fn main() {
+    let output = Command::new("/usr/bin/yq")
+        .arg("eval")
+        .arg(".window.opacity")
+        .arg("costam.yml")
+        .output()
+        .expect("failed to execute process");
 
-fn main() -> io::Result<()> {
-    let mut file = std::fs::OpenOptions::new()
-        .read(true)
-        .write(true)
-        .open("sample_file.yaml")
-        .expect("File should exist");
+    println!("Opacity: {}", String::from_utf8_lossy(&output.stdout));
 
-    let mut value: Value = serde_yaml::from_reader(&file).unwrap();
-
-    let pa = serde_yaml::to_string(&value["window"]["opacity"]).unwrap();
-
-    let aka = pa.trim().to_string();
-
-    println!("{:?}", &aka);
-
-    let toggle_match = matcher(aka);
-
-    println!("This is after toggle match call {:?}", toggle_match);
-
-    value["window"]["opacity"] = toggle_match.into();
-
-    file.rewind().unwrap();
-
-    serde_yaml::to_writer(&file, &value).unwrap();
-
-    Ok(())
-}
-fn matcher(x: String) -> String {
-    println!("{:?}", x.trim().to_string());
-    match x.trim() {
-        "'1.0'" => String::from("'0.9'"),
-        "'0.9'" => String::from("'1.0'"),
-        _ => String::from("'1.0'"),
-    }
+    Command::new("/usr/bin/sed")
+        .arg("-i")
+        .arg("s#  opacity: \\(.*\\)#  opacity: 1.0#")
+        .arg("costam.yml")
+        .spawn()
+        .expect("failed to execute process");
 }
