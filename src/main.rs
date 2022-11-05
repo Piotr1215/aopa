c43d13080b6b332248e27a4dd1a6e7900ce8d47e
Piotr Zaniewski <piotr@upbound.io>
2022-11-05 17:38:51 +0100

    Why don't u work?!

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,45 @@
-use std::{path::Path, process::Command};
+use std::{
+    env,
+    path::Path,
+    process::{self, Command},
+};
 
 fn main() {
-    println!("{}", Path::new("/usr/bin/seda").exists());
-    // TODO: Error handling for executables
+    if !Path::new("/usr/local/bin/yq").exists() || !Path::new("/usr/bin/sed").exists() {
+        print!("yq or sed are not present in the system, exiting");
+        process::exit(1)
+    }
+    let _home = env::var("HOME").unwrap_or_else(|_| "/home/decoder/".to_owned());
+
+    // let alacritty_config = Path::new(&home).join(".config/alacritty/alacritty.yml");
+    let alacritty_config = Path::new("/home/decoder/dev/rust/aopa/costam.yml");
+
     let output = Command::new("/usr/bin/yq")
         .arg("eval")
         .arg(".window.opacity")
-        // TODO: make sure file exists
-        .arg("costam.yml")
+        .arg(&alacritty_config)
         .output()
-        // TODO: Error handling with `Result`
         .expect("failed to execute process");
 
-    println!("Opacity: {}", String::from_utf8_lossy(&output.stdout));
+    let current_opacity = String::from_utf8_lossy(&output.stdout);
 
-    Command::new("/usr/bin/sed")
-        .arg("-i")
-        .arg("s#  opacity: \\(.*\\)#  opacity: 1.0#")
-        .arg("costam.yml")
-        .spawn()
-        .expect("failed to execute process");
+    println!("Current opacity {}", current_opacity);
+
+    if current_opacity == "0.9" {
+        println!("Current opacity {}", current_opacity);
+        Command::new("/usr/bin/sed")
+            .arg("-i")
+            .arg("s#  opacity: \\(.*\\)#  opacity: 0.9#")
+            .arg("costam.yml")
+            .spawn()
+            .expect("failed to execute process");
+    } else {
+        println!("Current opacity {}", current_opacity);
+        Command::new("/usr/bin/sed")
+            .arg("-i")
+            .arg("s#  opacity: \\(.*\\)#  opacity: 1.0#")
+            .arg("costam.yml")
+            .spawn()
+            .expect("failed to execute process");
+    }
 }
