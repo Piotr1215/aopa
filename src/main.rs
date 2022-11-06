482c555066da5a60d0b01b03babc31f4ab7bdb4f
Piotr Zaniewski <piotr@upbound.io>
2022-11-06 12:46:54 +0100

    Swapping to regex and removing Command based implementaiton

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,19 @@
+use regex::Regex;
 use std::{
     env,
+    fs::File,
+    io::{prelude::*, BufReader},
     path::Path,
-    process::{self, Command},
+    process,
 };
 
 const NO_OPACITY: &str = "1.0";
 const SMALL_OPACITY: &str = "0.9";
 const ALACRITY_PATH: &str = ".config/alacritty/alacritty.yml";
-const YQ_PATH: &str = "/usr/bin/yq";
-const SED_PATH: &str = "/usr/bin/sed";
 
-fn main() {
+fn main() -> std::io::Result<()> {
     if cfg!(target_os = "windows") {
         println!("This will not work on windows, get Linux instead!");
-        process::exit(1)
-    }
-
-    if !Path::new(YQ_PATH).exists() || !Path::new(SED_PATH).exists() {
-        print!("yq or sed are not present in the system, exiting");
         process::exit(1)
     }
 
@@ -26,29 +22,26 @@ fn main() {
 
     let alacritty_config = Path::new(&home).join(ALACRITY_PATH).display().to_string();
 
-    let output = Command::new(YQ_PATH)
-        .arg("eval")
-        .arg(".window.opacity")
-        .arg(&alacritty_config)
-        .output()
-        .expect("failed to execute process");
+    let file = File::open(&alacritty_config)?;
+    let mut buf_reader = BufReader::new(file);
+    let mut content = String::new();
+    buf_reader.read_to_string(&mut content)?;
 
-    let current_opacity = String::from_utf8_lossy(&output.stdout);
+    let regex = Regex::new("opacity: (.*)").unwrap();
+    let current_value = regex.captures(&content).unwrap().get(1).unwrap().as_str();
+    let mut f = File::create(&alacritty_config)?;
 
-    let new_opacity = if current_opacity.trim() == NO_OPACITY {
+    let new_opacity = if current_value.trim() == NO_OPACITY {
         SMALL_OPACITY
     } else {
         NO_OPACITY
     };
 
-    let sub: String = format!(r#"s#  opacity: \(.*\)#  opacity: {}#"#, new_opacity);
+    let after = regex.replace_all(&content, format!("opacity: {}", new_opacity));
+    f.write_all(after.as_bytes())?;
 
-    Command::new(SED_PATH)
-        .arg("-i")
-        .arg(sub)
-        .arg(alacritty_config)
-        .spawn()
-        .expect("failed to execute process");
+    f.sync_all()?;
 
     println!("Swapped to new opacity value: {}", new_opacity);
+    Ok(())
 }
